58d16fc0034608306c446b2b17e79e8f9e49bd00
danielpqb <danielpqb@gmail.com>
2022-12-01 19:32:37 -0300

    Removing catches that would never happen

--- src/controllers/bookings-controller.ts
+++ src/controllers/bookings-controller.ts
@@ -12,7 +12,6 @@ export async function getBooking(req: AuthenticatedRequest, res: Response) {
     return res.status(httpStatus.OK).send(booking);
   } catch (error) {
     if (error.name === "NotFoundError") return res.sendStatus(httpStatus.NOT_FOUND);
-    return res.sendStatus(httpStatus.BAD_REQUEST);
   }
 }
 
@@ -20,24 +19,20 @@ export async function postBooking(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
   const roomId = Number(req.body.roomId);
 
-  try {
-    const roomExists = await bookingsService.checkIfRoomIdExists(roomId);
-    if (!roomExists) return res.sendStatus(httpStatus.NOT_FOUND);
+  const roomExists = await bookingsService.checkIfRoomIdExists(roomId);
+  if (!roomExists) return res.sendStatus(httpStatus.NOT_FOUND);
 
-    const roomIsFull = await bookingsService.checkIfRoomIsFull(roomId);
-    if (roomIsFull) return res.sendStatus(httpStatus.FORBIDDEN);
+  const roomIsFull = await bookingsService.checkIfRoomIsFull(roomId);
+  if (roomIsFull) return res.sendStatus(httpStatus.FORBIDDEN);
 
-    const userCanBook = await bookingsService.checkIfUserCanBook(userId);
-    if (!userCanBook) return res.sendStatus(httpStatus.FORBIDDEN);
+  const userCanBook = await bookingsService.checkIfUserCanBook(userId);
+  if (!userCanBook) return res.sendStatus(httpStatus.FORBIDDEN);
 
-    const userHaveBooking = await bookingsService.checkIfUserAlreadyHaveBooking(userId);
-    if (userHaveBooking) return res.sendStatus(httpStatus.FORBIDDEN);
+  const userHaveBooking = await bookingsService.checkIfUserAlreadyHaveBooking(userId);
+  if (userHaveBooking) return res.sendStatus(httpStatus.FORBIDDEN);
 
-    const booking = await bookingsService.postBooking(userId, roomId);
-    return res.status(httpStatus.OK).send(booking);
-  } catch (error) {
-    return res.sendStatus(httpStatus.BAD_REQUEST);
-  }
+  const booking = await bookingsService.postBooking(userId, roomId);
+  return res.status(httpStatus.OK).send(booking);
 }
 
 export async function putBooking(req: AuthenticatedRequest, res: Response) {
@@ -45,22 +40,18 @@ export async function putBooking(req: AuthenticatedRequest, res: Response) {
   const bookingId = Number(req.params.bookingId);
   const roomId = Number(req.body.roomId);
 
-  try {
-    const roomExists = await bookingsService.checkIfRoomIdExists(roomId);
-    if (!roomExists) return res.sendStatus(httpStatus.NOT_FOUND);
+  const roomExists = await bookingsService.checkIfRoomIdExists(roomId);
+  if (!roomExists) return res.sendStatus(httpStatus.NOT_FOUND);
 
-    const roomIsFull = await bookingsService.checkIfRoomIsFull(roomId);
-    if (roomIsFull) return res.sendStatus(httpStatus.FORBIDDEN);
+  const roomIsFull = await bookingsService.checkIfRoomIsFull(roomId);
+  if (roomIsFull) return res.sendStatus(httpStatus.FORBIDDEN);
 
-    const userHaveBooking = await bookingsService.checkIfUserAlreadyHaveBooking(userId);
-    if (!userHaveBooking) return res.sendStatus(httpStatus.FORBIDDEN);
+  const userHaveBooking = await bookingsService.checkIfUserAlreadyHaveBooking(userId);
+  if (!userHaveBooking) return res.sendStatus(httpStatus.FORBIDDEN);
 
-    const userHaveBookingId = await bookingsService.checkIfUserHaveSpecificBookingId(userId, bookingId);
-    if (!userHaveBookingId) return res.sendStatus(httpStatus.FORBIDDEN);
+  const userHaveBookingId = await bookingsService.checkIfUserHaveSpecificBookingId(userId, bookingId);
+  if (!userHaveBookingId) return res.sendStatus(httpStatus.FORBIDDEN);
 
-    const booking = await bookingsService.putBooking(roomId, bookingId);
-    return res.status(httpStatus.OK).send(booking);
-  } catch (error) {
-    return res.sendStatus(httpStatus.BAD_REQUEST);
-  }
+  const booking = await bookingsService.putBooking(roomId, bookingId);
+  return res.status(httpStatus.OK).send(booking);
 }
